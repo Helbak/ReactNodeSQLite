c5bd422ccb7aca7734601a53c1759f26fb87eb4b
Helbak <helbak77@gmail.com>
2020-04-14 13:53:57 +0300

    deleted all

--- src/reactComponents/initReact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/App.jsx';
-
-export const init = () => {
-    const root = document.getElementById('root');
-const app = <div><App/></div>;
-    ReactDOM.render(app, root);
-};
